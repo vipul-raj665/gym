8a9b8b6f5f280d0bbb3e405335ff76f5657fac8a
BLACKBOXAI <blackboxai@example.com>
2026-06-10 13:39:27 +0530

    fix: import app styles in client entrypoint

--- src/client.tsx
+++ src/client.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./routes/index";
